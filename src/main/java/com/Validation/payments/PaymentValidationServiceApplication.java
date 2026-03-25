a764dea3f1102c03d9bd6f8381259cddaa9580b7
NiharMane <niharmane18@gmail.com>
2026-03-25 18:14:30 +0530

    initial version of code resolved all dependency problem

--- src/main/java/com/Validation/payments/PaymentValidationServiceApplication.java
+++ src/main/java/com/Validation/payments/PaymentValidationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.hulkhiretech.payments;
+package com.Validation.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
